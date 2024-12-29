70e2358dd6fa184144cec454c5f764719fc68186
SerzhSterl <133377073+SerzhSterl@users.noreply.github.com>
2024-12-29 05:57:59 +0300

    Update ReversedSequence.java

--- java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
+++ java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
@@ -1,5 +1,31 @@
 package exercise;
 
 // BEGIN
+public class ReversedSequence implements CharSequence {
+    private final String original;
 
+    public ReversedSequence(String input) {
+        this.original = input;
+    }
+
+    @Override
+    public int length() {
+        return original.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+        return original.charAt(length() - 1 - index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return new ReversedSequence(original.substring(length() - end, length() - start));
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder(original).reverse().toString();
+    }
+}
 // END
